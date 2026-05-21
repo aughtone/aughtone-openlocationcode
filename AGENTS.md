37550b576d12a7708ca88c2b7bba2811000ec5f5
Brill Pappin <brill@pappin.ca>
2026-05-21 11:35:02 -0400

    porting the original java to kotin

---
+++ AGENTS.md
@@ -0,0 +1,3 @@
+# Project Development Guide
+
+This document is the master instruction set for AI agents contributing to this repository.
---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    alias(libs.plugins.multiplatformLibrary) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply  false
+    alias(libs.plugins.vanniktech.mavenPublish) apply false
+}
