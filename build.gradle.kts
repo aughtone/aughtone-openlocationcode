271441b5e12a487bf443dee618a6a10f5719c71d
Brill Pappin <brill@pappin.ca>
2026-06-22 08:37:42 -0400

    build: configure Yarn lock file mismatches as warnings - Configure Kotlin YarnPlugin to report lock file mismatches as warnings instead of errors, matching the pattern in aughtone-types. - Enable automatic lock file replacement and disable reporting new lock files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,19 @@
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
+
 plugins {
     alias(libs.plugins.multiplatformLibrary) apply false
     alias(libs.plugins.kotlinMultiplatform) apply  false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
 }
+
+rootProject.plugins.withType<YarnPlugin> {
+    rootProject.the<YarnRootExtension>().apply {
+        lockFileDirectory = project.rootDir.resolve("kotlin-js-store")
+        yarnLockMismatchReport = org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport.WARNING
+        reportNewYarnLock = false
+        yarnLockAutoReplace = true
+    }
+}
+
 
